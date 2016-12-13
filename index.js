0befb176438fca75806cdd4e4742aebc67168bfc
Xuebing Wang <xbwang93@gmail.com>
2016-12-13 04:36:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback){
+  const array = [1, 2, 3, 4, 5, 6]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
